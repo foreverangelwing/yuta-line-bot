3efc1ba8beead2e52bf05e2cef87412f4a6b5024
foreverangelwing <foreverangelwing@gmail.com>
2021-08-09 07:08:55 +0800

    fix dict

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=msg + '產品說明書(TDS)' + r))
+            TextSendMessage(text=msg + '\n' + '產品說明書(TDS)' + r))
 
         
     
