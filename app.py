cc249ae1b74743b8a8b77199ab3368446ed10c56
foreverangelwing <foreverangelwing@gmail.com>
2021-08-09 08:15:21 +0800

    fix dict

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ def handle_message(event):
     r = '很抱歉，我不懂您在說什麼？'
 
     if msg in pd:
-        r = msg + 'PU雙液型壓克力樹脂' +'\n' + '產品說明書(TDS)' + '\n' + pd[msg]
+        if 'UR' in msg:
+            r = msg + ' PU雙液型壓克力樹脂' +'\n' + '產品說明書(TDS)' + '\n' + pd[msg]
        
         line_bot_api.reply_message(
             event.reply_token,
